308e45d5eff82b296265651b3acc093727a9c3c1
kananmzade0 <kananmzade0@gmail.com>
2025-03-30 06:16:17 +0900

    improved cleaning script

--- data/cleaning.py
+++ data/cleaning.py
@@ -1,4 +1,20 @@
-import sqlite3
 import pandas as pd
 
-pd.read_json('data/anime_list.json').to_sql('anime_list', sqlite3.connect('data/anime_list.db'))
+def clean_data():
+    df1 = pd.read_json("anime_full_details.json")
+    df2 = pd.read_json("anime_list.json")
+
+    df1['id'] = df1['link'].apply(lambda x: x.split("/anime/")[1].split("/")[0] if isinstance(x, str) and "/anime/" in x else None)
+    df2['id'] = df2['link'].apply(lambda x: x.split("/anime/")[1].split("/")[0] if isinstance(x, str) and "/anime/" in x else None)
+
+    df_merged = pd.merge(left=df1, right=df2, how='right', on=['id'], suffixes=(None, '_y'))
+    df_merged = df_merged.drop(columns=['title_y', 'link_y', 'score_y']) # drop duplicate columns that were added by the merge
+
+    df_merged = df_merged.drop_duplicates(subset=['id'])
+
+    df_merged['broadcast'] = df_merged['broadcast'].explode()
+
+    for i in df_merged.columns:
+        df_merged[i] = df_merged[i].apply(lambda x: x.replace("\n", "") if isinstance(x, str) else x)
+
+    return df_merged
